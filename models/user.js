e79e4b48021f8afb780e7b5b48860945ab303478
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-09-29 14:11:16 +0700

    Update code update semicolon :P

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   User.associate = function(models) {
     User.hasMany(models.Event);
-    user.hasMany(models.Joined)
+    user.hasMany(models.Joined);
   };
   return User;
 };
